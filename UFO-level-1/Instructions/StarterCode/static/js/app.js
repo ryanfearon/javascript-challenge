73a7ea6823ea380ad5f4a65d4052621169d0887b
ryanfearon <77812707+ryanfearon@users.noreply.github.com>
2021-05-02 13:03:01 -0400

    Update app.js

--- UFO-level-1/Instructions/StarterCode/static/js/app.js
+++ UFO-level-1/Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,26 @@
-// from data.js
+// from data.js, append into table function
 var tableData = data;
+function table(sampledata){
+var tbody = d3.select("tbody");
+tbody.html("");
+sampledata.forEach((data)=>{
+    var trow = tbody.append("tr")
+    Object.values(data).forEach((value)=>{
+var cells = trow.append("td");
+cells.text(value);
+    })
+})
+}
+table(tableData);
 
-// YOUR CODE HERE!
+//Filter table by data function
+function filterTable(){
+    var date = d3.select("#datetime").property("value");
+    if (date)
+{
+    filterData = tableData.filter(rowData=>rowData.datetime===date);
+
+}
+table(filterData)
+}
+d3.selectAll("#filter-btn").on("click",filterTable)
